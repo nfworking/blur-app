4269893c6f01f41221b58ea65f22929e749a49b7
nfworking <tidalfarmer1432@outlook.com>
2025-05-08 15:01:07 +1000

    Stage 2: changed app yaml name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:blur_app/main.dart';
+import 'package:markdown_notes/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
